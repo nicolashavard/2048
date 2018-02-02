cf5e63613d1f288f6fe74159131953f2bd5b3036
Edgear <nicolas.havard@epitech.eu>
2018-02-02 10:40:04 +0100

    gerer le cas des fleches

--- js/2048.js
+++ js/2048.js
@@ -1,3 +1,9 @@
+window.addEventListener("keydown", function(e) {
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 var new_grid = function () {
     var grid = $("#gridSelect").val();
     $("div#grid > div").remove();
